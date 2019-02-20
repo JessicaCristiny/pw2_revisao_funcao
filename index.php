7088140faf8435801a78e15062a25235ee05fe1b
jessi <jessi@LAPTOP-8JVJIOAK>
2019-02-20 13:55:31 -0300

    meu primeiro comitt

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>exercicio1</title>
+    </head>
+    <body>
+        <?php
+        echo "ola mundo";
+        ?>
+    </body>
+</html>
